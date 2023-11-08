a7f28d9ee237c0f91bc72d7eb175d03b821601ad
DenisShylov <dodjer123@gmail.com>
2023-11-08 15:40:29 +0200

    feat: add and create cache

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import { CssBaseline, ThemeProvider } from '@mui/material';
+import { CacheProvider } from '@emotion/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { cache } from './Shared/Cache';
 import { theme } from './Theme/Theme';
 import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ThemeProvider theme={theme}>
+    <CacheProvider value={cache}></CacheProvider>
     <CssBaseline />
     <App />
   </ThemeProvider>
